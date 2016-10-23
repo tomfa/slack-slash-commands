71c2f57afe6a1fdc22693a827c1f1faa2a78212c
Tomas Fagerbekk <tomas@webutvikling.org>
2016-10-23 18:55:37 +0200

    Demonstrate usage of slack payload

--- api/weather.js
+++ api/weather.js
@@ -1,31 +1,37 @@
 import { Router } from 'express';
 
-let router = Router();
+const router = Router();
 
 router.get('/', function(req, res) {
-    let response = {
+    const data = getPayLoad(req);
+    const response = {
         "text": "Now: 34 degrees and raining",
-        "attachments": [
-            {
-                "text":"Dummy GET response. Only you can see this"
-            }
-        ]
+        "attachments": [{
+            "text": `Dummy GET response. Only you can see this ${data.user_name}`
+        }]
     };
     res.send(response);
 });
 
 router.post('/', function(req, res) {
-    let response = {
+    const data = getPayLoad(req);
+    const response = {
         "response_type": "in_channel",
         "text": "Now: 34 degrees and raining",
-        "attachments": [
-            {
-                "text":"Dummy POST response. Everyone can see me now! :wave:"
-            }
-        ]
+        "attachments": [{
+            "text":`Thanks for asking ${data.user_name}. Hi everyone! :wave:`
+        }]
     };
     res.send(response);
 });
 
+/*
+    The following data is available (see https://api.slack.com/slash-commands)
+    token, team_id, team_domain, channel_id, channel_name, user_id, user_name,
+    command, text, response_url
+*/
+const getPayLoad = req => {
+    return req.method === 'GET' ? req.query : req.body;
+}
 
 module.exports = router;
