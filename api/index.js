f66e231f6f9497a1053a3ad5c0c137f5fe04ec54
Tomas Fagerbekk <tomas@webutvikling.org>
2016-10-23 18:55:20 +0200

    Remove duplicate dummy functionality

--- api/index.js
+++ api/index.js
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import weather from './weather';
-import transport from './transport';
 
 export default function() {
     let api = Router();
 
     api.use('/weather', weather);
-    api.use('/transport', transport);
 
     // perhaps expose some API metadata at the root
     api.get('/', (req, res) => {
