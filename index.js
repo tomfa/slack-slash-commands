02182c3163cb02dc784f69b1b1c7e7e4cfd756d4
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2016-10-25 17:02:15 +0200

    Remove silly amounts of whitespace

--- index.js
+++ index.js
@@ -4,35 +4,30 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 
 import { sslCheckResponse, slackAuth } from './middleware';
-
 import api from './api';
 
 var app = express();
-
-
 app.server = http.createServer(app);
 
-// Comment leftovers from earlier
 app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
+    // Set permissive CORS header - this allows this server to be used as
     // an API server
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Cache-Control', 'no-cache');
     next();
 });
-// parse application/x-www-form-urlencoded
+// parse application/x-www-form-urlencoded and json
 app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
 app.use(bodyParser.json())
+
+// use middleware
 app.use(sslCheckResponse, slackAuth);
 
+// enable api
 app.use('/', api());
 
-
-
+// start app
 app.server.listen(process.env.PORT || 8080);
-
 console.log(`Started on port ${app.server.address().port}`);
 
 export default app;
